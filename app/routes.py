04cce124edea642e44223ed0ad60a0bcd72a3e33
tsharksss <tsharksss@gmail.com>
2019-08-01 22:51:00 +0300

    update templates and style -_-

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, request, redirect, url_for
+
+
+@app.route('/en')
+def en_index():
+    return render_template('en_index.html')
 
 
 @app.route('/')
-def index():
-    return render_template('en_index.html')
+@app.route('/ru')
+def ru_index():
+    return render_template('ru_index.html')
+
+
+@app.route('/action', methods=['POST'])
+def action():
+    ITN = request.form.get('IndividualTaxNumber', None)
+    flash('Принял')
+    print(ITN)
+    return redirect(url_for('ru_index'))
