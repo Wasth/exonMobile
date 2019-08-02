fbdf8c83b48915cf2e6080aaf17b4ae07ff2970f
tsharksss <tsharksss@gmail.com>
2019-08-02 16:09:50 +0300

    (╯°□°）╯ send email and fix bugs -_-  ┻━┻

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
-from app import app
-from flask import render_template, flash, request, redirect, url_for
+import json
+from time import gmtime, strftime
+from app import app, mail
+from flask import render_template, request, redirect, url_for
+from flask_mail import Message
+from threading import Thread
 
 
 @app.route('/en')
@@ -13,11 +17,71 @@ def ru_index():
     return render_template('ru_index.html')
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
 @app.route('/action', methods=['POST'])
 def action():
     ITN = request.form.get('IndividualTaxNumber', None)
     TOE = request.form.get('TypeOfEntity', None)
+    CN = request.form.get('CompanyName', None)
+    AL = request.form.get('AddressLegal', None)
+    AA = request.form.get('AddressActual', None)
+    COIN = request.form.get('CountryOfIncorporationName', None)
+    RG = request.form.get('RussianGovernment', None)
+    RC = request.form.get('RussiansCitizens', None)
+    ORLE = request.form.get('OtherRussianLegalEntity', None)
+    TR = request.form.get('TotalRussian', None)
+    WEB = request.form.get('Website', None)
+    GSCN = request.form.get('GoodsServicesCommentsName', None)
+    GSLN = request.form.get('GoodsServicesListName', None)
+    SAAN = request.form.get('ServiceAreaAvailableName', None)
+    ACN = request.form.get('AnnualCashflowName', None)
 
-    flash('Принял')
-    print(ITN)
+    PCN = request.form.get('PrimaryContactName', None)
+    PJT = request.form.get('PrimaryJobTitle', None)
+    PCP = request.form.get('PrimaryContactPhone', None)
+    PE = request.form.get('PrimaryEMail', None)
+
+    SCN = request.form.get('SecondaryContactName', None)
+    SJT = request.form.get('SecondaryJobTitle', None)
+    SCP = request.form.get('SecondaryContactPhone', None)
+    SE = request.form.get('SecondaryEMail', None)
+
+    message = json.dumps({
+        'PrimaryContactName': PCN,
+        'PrimaryJobTitle': PJT,
+        'PrimaryContactPhone': PCP,
+        'PrimaryEMail': PE,
+
+        'SecondaryContactName': SCN,
+        'SecondaryJobTitle': SJT,
+        'SecondaryContactPhone': SCP,
+        'SecondaryEMail': SE,
+
+        'IndividualTaxNumber': ITN,
+        'TypeOfEntity': TOE,
+        'CompanyName': CN,
+        'AddressLegal': AL,
+        'AddressActual': AA,
+        'CountryOfIncorporationName': COIN,
+        'RussianGovernment': RG,
+        'RussiansCitizens': RC,
+        'OtherRussianLegalEntity': ORLE,
+        'TotalRussian': TR,
+        'Website': WEB,
+        'GoodsServicesCommentsName': GSCN,
+        'GoodsServicesListName': GSLN,
+        'ServiceAreaAvailableName': SAAN,
+        'AnnualCashflowName': ACN,
+        'Date': strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    })
+
+    subject = 'Your blank {}'.format(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    msg = Message(subject, sender=app.config['ADMINS'][0], recipients=PE.split())
+    msg.body = 'Your blank'
+    msg.html = message
+    Thread(target=send_async_email, args=(app, msg)).start()
     return redirect(url_for('ru_index'))
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('secret_key') or 'MyMostSecretKey'
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = '587'
+    MAIL_PORT = 587
     MAIL_USE_TLS = 1
     MAIL_USERNAME = 'mail.exxonmobile@gmail.com'
     MAIL_PASSWORD = '79eexon1999[]'
